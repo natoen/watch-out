1616470fe0a0ce1995f1c09b6d8ffc20a52ed75a
natoen <hgmatsuda@gmail.com>
2016-03-12 10:30:06 +0900

    (feat) Add gameOptions, gameStats, and board

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,23 @@
-// start slingin' some d3 here.
+var gameOptions = {
+  height: 450,
+  width: 700,
+  nEnemies: 30,
+  padding: 20
+};
+
+var gameStats = {
+  currentScore: 0,
+  highScore: 0,
+  collisions: 0
+}
+
+var addPixel = function(number) {
+  return number + 'px';
+}
+
+var board = d3.select('.board').append('svg')
+  .attr('width', gameOptions.width)
+  .attr('height', gameOptions.height);
+
+
+
